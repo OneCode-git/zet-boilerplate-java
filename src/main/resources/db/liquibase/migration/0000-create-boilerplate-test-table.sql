443ace94a40aaf5098b199d77915e99c9ec87bb0
pranjlpz <pranjal.pandey@onecode.in>
2023-06-02 11:42:26 +0530

    minor changes

--- src/main/resources/db/liquibase/migration/0000-create-boilerplate-test-table.sql
+++ src/main/resources/db/liquibase/migration/0000-create-boilerplate-test-table.sql
@@ -1,6 +1,10 @@
--- liquibase formatted sql
+--liquibase formatted sql
+/*
+  Created on: 0000-00-00 (YYYYY-MM-DD)
+*/
 
--- ignore
+--changeset pranjlpz:1
+--comment demonstrating a test table creation
 CREATE TABLE IF NOT EXISTS test (
   id BIGINT PRIMARY KEY,
   created_at DATETIME NOT NULL
